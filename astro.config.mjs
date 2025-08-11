75610e198cd39df6f72bb2f05bb281e3134ee94a
arnadame <adameaaron25@gmail.com>
2025-08-11 14:40:53 +0800

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arnadame.github.io',
   base: 'web-portfolio',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
